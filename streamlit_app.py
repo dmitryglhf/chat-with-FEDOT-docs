00c07b656168d5a8bd5103b6ea5726e76b96c5c4
Dmitry Gilemkhanov <dima.rize@yandex.ru>
2025-02-05 05:34:06 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
+import os
 import streamlit as st
 import google.generativeai as genai
+from llama_index.core.chat_engine import SimpleChatEngine
 from llama_index.llms.gemini import Gemini
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
 
@@ -10,6 +12,9 @@ st.set_page_config(
     initial_sidebar_state="auto",
     menu_items=None
 )
+
+GOOGLE_API_KEY = st.secrets.genai_key
+os.environ["GOOGLE_API_KEY"] = GOOGLE_API_KEY
 
 genai.configure(api_key=st.secrets.genai_key)
 st.title("Chat with the FEDOT docs")
@@ -27,6 +32,7 @@ if "messages" not in st.session_state.keys():  # Initialize the chat messages hi
 def load_data():
     reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
     docs = reader.load_data()
+    Settings.llm = None
     Settings.llm = Gemini(
         model="models/gemini-1.5-flash",
         api_key=st.secrets.genai_key,
@@ -47,7 +53,9 @@ index = load_data()
 
 if "chat_engine" not in st.session_state.keys():  # Initialize the chat engine
     st.session_state.chat_engine = index.as_chat_engine(
-        chat_mode="condense_question", verbose=True, streaming=True
+        verbose=True,
+        streaming=True,
+        llm=Settings.llm
     )
 
 if prompt := st.chat_input(
